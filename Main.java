4fb0ffbd943c2d4879dbea09d4f5511de4ea04cb
epgfm <recon@vlay.org>
2021-04-12 11:04:42 +0200

    Added StopWatch.java in time subpackage

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
 import myutils.*;
+import myutils.time.*;
 
 class Main {
     public static void main(String[] args) {
+        StopWatch c = new StopWatch();
+        c.start();
         int[] t = {1, 2, 4};
         Array.print(t);
 
-        int[] t2 = Array.genArray(20);
+        int[] t2 = Array.genArray(20000);
         Array.print(t2);
+
+        File.cat("README.md");
+        Time.print();
+        c.stop();
+        System.out.println(c);
     }
 }
